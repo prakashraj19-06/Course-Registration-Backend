016b606e8c17347bdba70ee278e5c0413b2b0620
prakashraj <prakashraj.sa2018@gmail.com>
2026-06-12 01:54:34 +0530

    update cross origin

--- src/main/java/demo/Course_Registration/controller/CourseController.java
+++ src/main/java/demo/Course_Registration/controller/CourseController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://127.0.0.1:5500/")
+@CrossOrigin(origins = "*")
 public class CourseController {
 
     @Autowired
